047093b3ec3658cce83db0c252fccf31fd338bba
Teng-hao Chang <zhusee2@gmail.com>
2021-05-18 11:20:25 +0800

    feat: prevent request exsting and more than expected count

--- src/index.js
+++ src/index.js
@@ -49,7 +49,7 @@ function randomPick(pickCount, candidates = []) {
   return results;
 }
 
-async function getReviewers(username) {
+async function getReviewers(username, initialReviewers = []) {
   const reviewers = [];
   const config = await fetchAndParseReviewers();
   const targetCount = core.getInput('count');
@@ -58,22 +58,29 @@ async function getReviewers(username) {
 
   // 先抽 mentor
   if (mentor) {
-    core.info(`Requesting mentor: ${mentor}`);
-    reviewers.push(mentor);
+    if (initialReviewers.includes(mentor)) {
+      core.info(`Mentor ${mentor} already requested.`);
+    } else {
+      core.info(`Requesting mentor: ${mentor}`);
+      reviewers.push(mentor);
+    }
   } else {
     core.info(`No mentor found.`);
   }
 
   // 再從同專案團隊優先抽滿
-  const belongingTeamMembers = Object.values(config.teams)
-    .find(teamMembers => teamMembers.includes(username))
-    .filter(member => member !== username && member!== mentor);
+  {
+    const belongingTeamMembers = Object.values(config.teams)
+      .find(teamMembers => teamMembers.includes(username))
+      .filter(member => member !== username);
 
-  const remainingCount = targetCount - reviewers.length;
-  const teamReviewers = randomPick(remainingCount, belongingTeamMembers);
-  reviewers.push(...teamReviewers);
+    const candidates = _.difference(belongingTeamMembers, reviewers, initialReviewers);
+    const remainingCount = targetCount - reviewers.length;
+    const teamReviewers = randomPick(remainingCount, candidates);
+    reviewers.push(...teamReviewers);
 
-  core.info(`Requesting team members: ${teamReviewers}`);
+    core.info(`Requesting team members: ${teamReviewers}`);
+  }
 
   // 不夠的話從 mentorship group 抽到滿
   if (reviewers.length < 2) {
@@ -81,7 +88,7 @@ async function getReviewers(username) {
       .find(group => group.includes(username))
       .filter(member => member !== username);
 
-    const candidates = _.difference(mentorshipGroupMembers, reviewers);
+    const candidates = _.difference(mentorshipGroupMembers, reviewers, initialReviewers);
     const remainingCount = targetCount - reviewers.length;
     const groupReviewers = randomPick(remainingCount, candidates);
 
@@ -95,17 +102,39 @@ async function getReviewers(username) {
 
 async function run() {
   const context = github.context;
+  const pullRequest = {
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    pull_number: context.payload.pull_request.number,
+  };
 
   try {
+    const targetCount = core.getInput('count');
     const author = context.payload.pull_request.user.login;
-    const reviewers = await getReviewers(author);
 
-    core.info(`Requested reviewers: ${reviewers}`);
+    const reviewedUsers = await octokit.pulls.listReviews(pullRequest)
+      .then(result => result.data.map(review => review.user.login))
+      .then(_.uniq)
+      .then(usernames => _.without(usernames, author));
+
+    const requestedUsers = await octokit.pulls.listRequestedReviewers(pullRequest)
+      .then(result => result.data.users.map(user => user.login));
+
+    const alreadyInvolvedUsers = [...reviewedUsers, ...requestedUsers];
+
+    if (alreadyInvolvedUsers.length) {
+      core.info(`Currently requested users: ${alreadyInvolvedUsers}`);
+    }
+
+    if (alreadyInvolvedUsers.length >= targetCount) {
+      core.info(`Already meet target ${targetCount} reviewers.`);
+      return;
+    }
+
+    const reviewers = await getReviewers(author, alreadyInvolvedUsers);
 
     await octokit.pulls.requestReviewers({
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      pull_number: context.payload.pull_request.number,
+      ...pullRequest,
       reviewers,
     });
   } catch (error) {
