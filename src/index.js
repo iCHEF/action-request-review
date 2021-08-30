75e45a2d60d6e99362e7c92cd4a17589ee34f210
Teng-hao Chang <zhusee2@gmail.com>
2021-08-30 17:39:42 +0800

    feat: exclude PRs from ichefbot

--- src/index.js
+++ src/index.js
@@ -46,7 +46,7 @@ async function getReviewLoadingOfUser(username, menteesList = []) {
     dateFns.sub(new Date, { weeks: 2 }),
     { representation: 'date' }
   );
-  const baseCriteria = `is:pr user:iCHEF created:>${dateOf2WeeksAgo} -label:"auto-pr" -label:"merge request" -author:app/github-actions`;
+  const baseCriteria = `is:pr user:iCHEF created:>${dateOf2WeeksAgo} -label:"auto-pr" -label:"merge request" -author:app/github-actions -author:ichefbot`;
 
   const {
     data: { total_count: countOfRequestedPulls },
